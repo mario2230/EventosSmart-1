3246a5e91460923caa5e8ee6785e7179fe870ade
mario2230 <124641210+mario2230@users.noreply.github.com>
2026-04-01 14:26:22 -0300

    feat(telas e funcionalidade): Ultima atualização no trabalho criado a tela de perfil salvando usando ref e watch.

--- src/views/PerfilPage.vue
+++ src/views/PerfilPage.vue
@@ -1,36 +1,79 @@
 <template>
   <ion-page>
 
-      <ion-toolbar>
-        <ion-title>Perfil</ion-title>
-      </ion-toolbar>
-
+    <ion-toolbar>
+      <ion-title>Perfil</ion-title>
+    </ion-toolbar>
 
     <ion-content :fullscreen="true">
-      <ion-grid class="ion-no-padding" style="height: 100%;">
-        <ion-row class="ion-justify-content-center ion-align-items-center" style="height: 100%;">
-          <ion-col size="12" class="ion-text-center ion-padding">
-            
-            <p>
-              Tela de Perfil!
-            </p>
+      <ion-grid class="ion-padding">
 
+        <ion-row>
+          <ion-col size="12">
+            <ion-input 
+              label="Nome"
+              label-placement="floating"
+              v-model="usuario.nome"
+            />
           </ion-col>
         </ion-row>
+
+        <ion-row>
+          <ion-col size="12">
+            <ion-input 
+              label="Email"
+              label-placement="floating"
+              v-model="usuario.email"
+            />
+          </ion-col>
+        </ion-row>
+
+        <ion-row class="ion-margin-top">
+          <ion-col size="12" class="ion-text-center">
+            <ion-button @click="salvarPerfil">
+              Salvar
+            </ion-button>
+          </ion-col>
+        </ion-row>
+
       </ion-grid>
     </ion-content>
+
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { 
-  IonPage, 
-  IonToolbar, 
-  IonTitle, 
+import {
+  IonPage,
+  IonToolbar,
+  IonTitle,
   IonContent,
   IonGrid,
   IonRow,
-  IonCol 
-} from '@ionic/vue';
-</script>
+  IonCol,
+  IonInput,
+  IonButton
+} from '@ionic/vue'
 
+import { ref, watch } from 'vue'
+
+const usuario = ref({
+  nome: '',
+  email: ''
+})
+
+const dadosSalvos = localStorage.getItem('perfil')
+if (dadosSalvos) {
+  usuario.value = JSON.parse(dadosSalvos)
+}
+
+watch(usuario, (novoValor) => {
+  console.log('Perfil atualizado:', novoValor)
+}, { deep: true })
+
+
+function salvarPerfil() {
+  localStorage.setItem('perfil', JSON.stringify(usuario.value))
+  alert('Perfil salvo!')
+}
+</script>
